9e61bce35b8e0f0cdf525bfc8b9bb77efe4f8718
Sixuan Wu <wusixuan2@icloud.com>
2024-08-01 15:45:00 -0400

    update url http://127.0.0.1:8000/api/hello/

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,5 @@ from .views import HelloWorld
 from django.urls import path, include
 
 urlpatterns = [
-    path('hello/', HelloWorld.as_view(), name='hello_world'),
+    path('hello/', HelloWorld.as_view(), name='hello_world')
 ]
