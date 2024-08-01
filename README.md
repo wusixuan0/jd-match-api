8a8711bce0fea667e271f005b16fd11d0b0fab27
Sixuan Wu <wusixuan2@icloud.com>
2024-08-01 16:03:30 -0400

    config db connection to postgres DATABASE_URL using doppler, dj-database-url, psycopg2

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
+quick start
+```
+source venv/bin/activate
+doppler run -- python manage.py runserver
+```
+http://127.0.0.1:8000/api/hello/  
+get started
+```
+python3 -m venv venv
+source venv/bin/activate
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+export DOPPLER_TOKEN='dp.st.dev.'
+doppler run --config dev -- python manage.py runserver
+```
+instead of token
+```
+doppler login
+doppler setup
+```
+```
+export DOPPLER_TOKEN='dp.st.prd'
+```  
+doppler get started:  
+https://github.com/wusixuan0/seed-supabase/blob/main/README.md  
+
+tutorial steps:  
 ```
 touch django-api
 cd django-api
@@ -32,7 +59,12 @@ my-project/
 └── requirements.txt
 ```
 
+
 ```
 touch api/urls.py
 
 ```
+add app route to config/urls.py  
+add Hello World to api/urls.py  
+http://127.0.0.1:8000/api/hello/
+
--- config/settings.py
+++ config/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from dopplersdk import DopplerSDK
+import os
 from pathlib import Path
+import dj_database_url
+
+doppler = DopplerSDK()  
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,12 +80,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    ),
 }
 
+from django.db import connections
+def check_database_connection():
+    connections['default'].cursor()
+    print(f"Database connected: {os.getenv('DATABASE_URL')}")
+
+check_database_connection()
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
