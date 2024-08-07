85a3ed1b7c405906b763fe1045505f5caa87e71d
Sixuan Wu <wusixuan2@icloud.com>
2024-08-07 15:45:07 -0400

    modify match record model

--- api/models.py
+++ api/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 class MatchRecord(models.Model):
+    resume_url =  models.URLField()
     resume_summary = models.TextField()
-    job_id_list = models.CharField()
+    job_id_list = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
