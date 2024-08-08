d0043907ac5f25ade5768aec24bb552e37d56aea
Sixuan Wu <wusixuan2@icloud.com>
2024-08-07 23:19:03 -0400

    configure render

--- config/settings.py
+++ config/settings.py
@@ -81,14 +81,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        conn_health_checks=True,
-    ),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.environ.get('DATABASE_URL'),
+#         conn_max_age=600,
+#         conn_health_checks=True,
+#     ),
+# }
 
 from django.db import connections
 def check_database_connection():
