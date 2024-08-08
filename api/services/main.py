68761052cd9fe32064fdd3e3fef7e80195f72da7
Sixuan Wu <wusixuan2@icloud.com>
2024-08-07 23:35:53 -0400

    fix render

--- api/services/main.py
+++ api/services/main.py
@@ -1,19 +1,14 @@
 from .process_resume import extract_resume
 from .es_query_resume import retrieve_jd
-from .example_data import es_example, resume_summary_example, rank_example
 from .match_and_rank import rank_result
 from .es_query_jd_id import retrieve_jds
 
 USE_API = False
-USE_ES = False
 
 def resume_service(resume_url):
     if USE_API:
         resume_summary = extract_resume(resume_url)
-    else:
-        resume_summary=resume_summary_example()
 
-    if USE_ES:
         es_query_data = {
             "target job titles": resume_summary.get('target job titles'),
             "skills": resume_summary.get('skills'),
@@ -21,11 +16,6 @@ def resume_service(resume_url):
         }
 
         jd_list = retrieve_jd(es_query_data)
-        
-    else:
-        jd_list = es_example()
-
-    if USE_API:
         rank_id_list=rank_result(resume_summary, jd_list)
     else:
         rank_example = [
