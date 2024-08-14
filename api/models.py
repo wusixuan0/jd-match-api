2f86a72b94e87993ec3c655d9c033c78a9301d19
Sixuan Wu <wusixuan2@icloud.com>
2024-08-14 16:22:36 -0400

    add model

--- api/models.py
+++ api/models.py
@@ -1,11 +1,28 @@
 from django.db import models
 
-class MatchRecord(models.Model):
+class TemporaryTransaction(models.Model):
+    file_url =  models.URLField()
+    file_summary = models.TextField()
+    ranked_ids = models.CharField(max_length=255)
+
+class UserEmail(models.Model):
+    email = models.EmailField(unique=True)
+    frequency = models.CharField(max_length=20)
+
+class Resume(models.Model):
+    user_email = models.ForeignKey(UserEmail, on_delete=models.CASCADE)
     resume_url =  models.URLField()
     resume_summary = models.TextField()
-    job_id_list = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return f"MatchRecord {self.id}"
+class JobRecommendation(models.Model):
+    user_email = models.ForeignKey(UserEmail, on_delete=models.CASCADE)
+    resume = models.ForeignKey(Resume, on_delete=models.CASCADE)
+    ranked_job_ids = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class UserFeedback(models.Model):
+    user_email = models.ForeignKey(UserEmail, on_delete=models.CASCADE)
+    applied_job_ids = models.CharField(max_length=255)
+    user_ranking = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
