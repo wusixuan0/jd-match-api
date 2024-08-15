b35fb1e4717e2616080e7421b6ada99dd27205eb
Sixuan Wu <wusixuan2@icloud.com>
2024-08-14 21:07:08 -0400

    modify UserFeedback table to save feedback from both users who subscribe and don't subscribe.

--- api/models.py
+++ api/models.py
@@ -22,7 +22,20 @@ class JobRecommendation(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
 class UserFeedback(models.Model):
-    user_email = models.ForeignKey(UserEmail, on_delete=models.CASCADE)
+    user_email = models.ForeignKey(UserEmail, on_delete=models.SET_NULL, null=True, blank=True)
+    temporary_transaction = models.ForeignKey(TemporaryTransaction, on_delete=models.SET_NULL, null=True, blank=True)
     applied_job_ids = models.CharField(max_length=255)
-    user_ranking = models.CharField(max_length=255)
+    user_ranking = models.CharField(max_length=255, null=True, blank=True)
+    feedback_text = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=(
+                    models.Q(temporary_transaction__isnull=False) |
+                    models.Q(user__isnull=False)
+                ),
+                name='feedback_has_temporary_transaction_or_user'
+            )
+        ]
